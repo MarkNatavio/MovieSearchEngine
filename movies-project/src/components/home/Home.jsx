b18bf10988a79d01b0f388c8b34ed85308531b5e
Mark Natavio <43160780+MarkNatavio@users.noreply.github.com>
2022-03-10 21:22:05 -0500

    Adding react file

---
+++ movies-project/src/components/home/Home.jsx
@@ -0,0 +1,123 @@
+import React, { useState, useEffect } from "react";
+import { fetchGenre, fetchMovieByGenre, movieSearch } from "../../service/index";
+import Footer from '../Footer'
+import "react-bootstrap-carousel/dist/react-bootstrap-carousel.css";
+import { Link } from "react-router-dom";
+
+export function Home() {
+
+  let inputText = '';
+
+  const [genres, setGenres] = useState([]);
+  const [movieByGenre, setMovieByGenre] = useState([]);
+  const [movieTitle, setSearchTitle] = useState([]);
+
+  useEffect(() => {
+    const fetchAPI = async () => {
+      setGenres(await fetchGenre());
+      setMovieByGenre(await fetchMovieByGenre(0));
+      setSearchTitle(await movieSearch("$"));
+    };
+
+    fetchAPI();
+  }, []);
+
+  // Searching movies of specific genre
+  const handleGenreClick = async (genre_id) => {
+    setMovieByGenre(await fetchMovieByGenre(genre_id));
+    setSearchTitle(await movieSearch("$"));
+  };
+
+  // Searching movies based on keyword
+  const handleMovieSearch = async () => {
+    setSearchTitle(await movieSearch(inputText));
+    setMovieByGenre(await fetchMovieByGenre(0));
+  }
+
+  const handleNewKeyword = (event) => {
+    inputText = event.target.value;
+  }
+  
+  const moviesSearched = movieTitle.map((item, index) => {
+    return (
+      <div className="col-md-3 col-sm-6" key={index}>
+        <div className="card">
+          <Link to={`/movie/${item.id}`}>
+            <img className="img-fluid" src={item.poster} alt={item.title}></img>
+          </Link>
+        </div>
+        <div className="mt-3">
+          <p style={{ fontWeight: "bolder" }}>{item.title}</p>
+          <p>Rated: {item.rating}</p>
+        </div>
+      </div>
+    );
+  });
+
+  const genreList = genres.map((item, index) => {
+    return (
+      <li className="list-inline-item" key={index}>
+        <button
+          type="button"
+          style={{color:'gold'}}
+          className="btn btn-outline-info"
+          onClick={() => {
+            handleGenreClick(item.id);
+          }}
+        >
+          {item.name}
+        </button>
+      </li>
+    );
+  });
+
+  const movieList = movieByGenre.map((item, index) => {
+    return (
+      <div className="col-md-3 col-sm-6" key={index}>
+        <div className="card">
+          <Link to={`/movie/${item.id}`}>
+            <img className="img-fluid" src={item.poster} alt={item.title}></img>
+          </Link>
+        </div>
+        <div className="mt-3">
+          <p style={{ fontWeight: "bolder" }}>{item.title}</p>
+          <p>Rated: {item.rating}</p>
+        </div>
+      </div>
+    );
+  });
+
+  return (
+    <div className="container">
+      <div className="row mt-4">
+        <div className="col-1">
+            <h1 style={{color: 'white'}}>Home</h1>
+        </div>
+      </div>
+
+      <hr style={{ borderTop: "1px solid #5a606b" }}></hr>
+      
+      <h3 className="mt-3" style={{color: 'white'}}>Search</h3>
+      <form>
+          <input type='text' placeholder='Looking for something?' onChange={handleNewKeyword}></input>
+          <button type="button" style={{color:'gold'}} className="btn btn-outline-info"
+            onClick={ handleMovieSearch }>Enter</button>
+      </form>
+      
+      <div className="row mt-3">
+        <div className="col">
+          <h3 className="mt-3" style={{color: 'white'}}>Genres:</h3>
+          <ul className="list-inline">{genreList}</ul>
+        </div>
+      </div>
+
+      <hr style={{ borderTop: "1px solid #5a606b" }}></hr>
+      <h3 style={{color: 'white'}}>{"Movies"}</h3>
+
+      <div className="row mt-3">{moviesSearched}</div>
+      <div className="row mt-3">{movieList} </div>
+	  <hr className="mt-5" style={{ borderTop: "1px solid #5a606b" }}></hr>
+	  <Footer/>
+    </div>
+  );
+}
---
+++ movies-project/src/service/index.js
@@ -0,0 +1,147 @@
+import axios from 'axios';
+
+// Constants for the Movie Database (TMDB) API
+const key = 'd681d6302c1d36abb2ea52e15f7ba6a8'; // API key
+const URL = 'https://api.themoviedb.org/3'; // basic API URL 
+const genreUrl = `${URL}/genre/movie/list`; // URL to find movies by genre
+const moviesUrl = `${URL}/discover/movie`; // URL to find similar movies
+const movieUrl = `${URL}/movie`; // URL to find info on movie
+const search = `${URL}/search/movie?api_key=${key}` // URL to find movies based on key word
+
+// Get array of all genres
+export const fetchGenre = async () => {
+    try {
+        const { data } = await axios.get(genreUrl, {
+            params: {
+                api_key: key,
+                language: 'en_US',
+                page: 1
+            }
+        })
+        const modifiedData = data['genres'].map((g) => ({
+            id: g['id'],
+            name: g['name']
+        }))
+
+        return modifiedData;
+    } catch (error) { }
+}
+
+// Get array of all movies of a genre
+export const fetchMovieByGenre = async (genre_id) => {
+    try {
+        const { data } = await axios.get(moviesUrl, {
+            params: {
+                api_key: key,
+                language: 'en_US',
+                with_genres: genre_id
+            }
+        })
+        const posterUrl = 'https://image.tmdb.org/t/p/original/';
+        const modifiedData = data['results'].map((m) => ({
+            id: m['id'],
+            backPoster: posterUrl + m['backdrop_path'],
+            popularity: m['popularith'],
+            title: m['title'],
+            poster: posterUrl + m['poster_path'],
+            overview: m['overview'],
+            rating: m['vote_average'],
+        }))
+
+        return modifiedData;
+    } catch (error) { }
+}
+
+// Get data on movie selected
+export const fetchMovieDetail = async (id) => {
+    try {
+        const { data } = await axios.get(`${movieUrl}/${id}`, {
+            params: {
+                api_key: key,
+                language: 'en_US'
+            }
+        });
+
+        return data;
+    } catch (error) { }
+}
+
+// Get videos of the movie (the trailer)
+export const fetchMovieVideos = async (id) => {
+    try {
+        const { data } = await axios.get(`${movieUrl}/${id}/videos`, {
+            params: {
+                api_key: key,
+            }
+        });
+        return data['results'][0];
+    } catch (error) { }
+}
+
+// Get data on movie cast
+export const fetchCasts = async (id) => {
+    try {
+        const { data } = await axios.get(`${movieUrl}/${id}/credits`, {
+            params: {
+                api_key: key,
+            }
+        });
+        const modifiedData = data['cast'].map((c) => ({
+            id: c['cast_id'],
+            character: c['character'],
+            name: c['name'],
+            img: 'https://image.tmdb.org/t/p/w200' + c['profile_path'],
+        }))
+
+        return modifiedData;
+    } catch (error) { }
+}
+
+// Get array of similar movies
+export const fetchSimilarMovie = async (id) => {
+    try {
+        const { data } = await axios.get(`${movieUrl}/${id}/similar`, {
+            params: {
+                api_key: key,
+                language: 'en_US'
+            }
+        });
+        const posterUrl = 'https://image.tmdb.org/t/p/original/';
+        const modifiedData = data['results'].map((m) => ({
+            id: m['id'],
+            backPoster: posterUrl + m['backdrop_path'],
+            popularity: m['popularith'],
+            title: m['title'],
+            poster: posterUrl + m['poster_path'],
+            overview: m['overview'],
+            rating: m['vote_average'],
+        }))
+
+        return modifiedData;
+    } catch (error) { }
+}
+
+// Get array of movies based on key word search
+export const movieSearch = async (title) => {
+    try {
+        const { data } = await axios.get(`${search}&query=${title}`, {
+            params: {
+                api_key: key,
+                language: 'en_US'
+            }
+        })
+
+        const posterUrl = 'https://image.tmdb.org/t/p/original/';
+        const modifiedData = data['results'].map((m) => ({
+            id: m['id'],
+            backPoster: posterUrl + m['backdrop_path'],
+            popularity: m['popularith'],
+            title: m['title'],
+            poster: posterUrl + m['poster_path'],
+            overview: m['overview'],
+            rating: m['vote_average'],
+        }))
+
+        return modifiedData;
+    } catch (error) { }
+}
