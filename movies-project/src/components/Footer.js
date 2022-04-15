fe253f0f136a44a01f066016a019d91d99098f7d
Mark Natavio <43160780+MarkNatavio@users.noreply.github.com>
2022-04-15 17:25:55 -0400

    cache changes

--- movies-project/src/components/Footer.js
+++ movies-project/src/components/Footer.js
@@ -8,50 +8,6 @@ import {
     Heading,
   } from "./FooterStyles";
 
-/*
-const Footer = () => {
-    return (
-        <div className="col-mt-3 col-mb-5">
-            <h3 style={{color: 'white'}}>Contact Info</h3>
-            <ul className="list-unstyled">
-                <li>
-                <p>
-                    <strong>
-                    <i className="fas fa-map-marker-alt"></i> Phone:
-                    </strong>
-                </p>
-                </li>
-                <li>
-                <p>
-                    <strong>
-                    <i className="fas fa-envelope"></i> Email:
-                    </strong>
-                </p>
-                </li>
-            </ul>
-            
-            <div className="row mt-3 mb-5">
-            <div className="col-md-8 col-sm-6" style={{ color: "#5a606b" }}>
-                <ul className="list-inline">
-                <li className="list-inline-item">
-                    <a href="/" style={{ color: "gold" }}>
-                    <i className="fab fa-facebook"></i>
-                    </a>
-                </li>
-                <li className="list-inline-item">
-                    <a href="/" style={{ color: "gold" }}>
-                    <i className="fab fa-instagram"></i>
-                    </a>
-                </li>
-                 </ul>
-             </div>
-             </div>
-         </div>
-    );
-}
-*/
-// export default Footer;
-
 const Footer = () => {
   return (
     <Box>
